416d008adaa002259eabb7fda1faa520d02fefe5
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-29 19:19:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Device from './components/Device/Device';
+
+function App() {
+  return (
+    <div className="App">
+      <Device name='u-phone' price = '9350'></Device>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DeviceDetails/DeviceDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeviceDetails = (props) => {
+    return (
+        <div>
+            <h1>price : {props.price}</h1>
+        </div>
+    );
+};
+
+export default DeviceDetails;
