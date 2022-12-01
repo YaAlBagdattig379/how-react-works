763390c111d52b39bba2755149994486b3b819a7
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-12-01 13:58:57 +0530

    added

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Dial from '../Dial/Dial';
+import Tablet from '../Tablet/Tablet';
 
-const Display = (props) => {
+const Display = (props) => { 
     return (
         <div>
             <h3>hello good morning at : {props.time}</h3>
             <h3>i am away from : {props.mtr}mtr</h3>
             <Dial running ={props.mtr}></Dial>{/* 'running with Props.mtr' are to send into dial component
             */}
+            <Tablet></Tablet>{/*this react class component call in here  */}
             
         </div>
     );
---
+++ src/components/Tablet/Tablet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Tablet extends Component {///*this react class component example
+    render() {
+        return (
+            <div>
+                <h1>o hello ! react class component</h1>
+            </div>
+        );
+    }
+}
+
+export default Tablet;
