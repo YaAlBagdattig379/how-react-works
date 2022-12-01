a0c46dbbca6170ad3d44b2b1fbfdea4aa5c00794
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-12-01 11:22:35 +0530

    added

--- src/components/Watch/Watch.js
+++ src/components/Watch/Watch.js
@@ -5,10 +5,17 @@ const Watch = () => {
 //   stats declare
 const [mtr,setMtr] = useState(0);
 // annonymous arrow function 
-const running = () =>{
+const running = () =>{ //for go ahead 
     const newMtr = mtr + 1;
     setMtr(newMtr)
 }
+// const running2 = () =>{ //for go back 
+//     if(mtr > 0){
+//         const newMtr = mtr - 1;
+//         setMtr(newMtr)
+//     }
+    
+// }
     return (
         <div style={{backgroundColor:'olivedrab',border:'5px solid green',margin:'10px',padding:'10px'}}>
             <h1>This is her Smart Watch</h1>
@@ -16,6 +23,9 @@ const running = () =>{
 
             <button onClick={running} style={{borderRadius:'7px',borderStyle:'none',backgroundColor:'blue',color:'white'}}>de dour.....</button>
             <Display time='11am' mtr={mtr} ></Display>{/*data are to send into display component*/}
+{/*         
+            <button onClick={running2} style={{borderRadius:'7px',marginTop:'10px',borderStyle:'none',backgroundColor:'blue',color:'white'}}>pichone dour.....</button>
+            <Display time='11am' mtr={mtr} ></Display>data are to send into display component */}
         
         </div>
     );
